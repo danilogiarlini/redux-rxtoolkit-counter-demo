23824cf74e36a930b475f7abf0bbccf0482e9cfa
Danilo Giarlini <danilogiarlini@gmail.com>
2023-09-29 16:40:45 +0200

    project finished

---
+++ src/store/index.js
@@ -0,0 +1,33 @@
+import { configureStore } from "@reduxjs/toolkit";
+import authReducer from './auth'
+import counterReducer from './counter'
+
+// const counterReducer = (state = initialState, action) => {
+//   if (action.type === "increment") {
+//     return { counter: state.counter + 1, showCounter: state.showCounter };
+//   }
+
+//   if (action.type === "increase_by_5") {
+//     return {
+//       counter: state.counter + action.amount,
+//       showCounter: state.showCounter,
+//     };
+//   }
+
+//   if (action.type === "decrement") {
+//     return { counter: state.counter - 1, showCounter: state.showCounter };
+//   }
+
+//   if (action.type === "toggle") {
+//     return { counter: state.counter, showCounter: !state.showCounter };
+//   }
+
+//   return state;
+// };
+
+
+const store = configureStore({
+  reducer: { counter: counterReducer, auth: authReducer },
+});
+
+export default store;
